fb69f2e52bcf2300a83160cbfa74f4791e9390d2
alexandergf <alexgferreiro@hotmail.com>
2021-03-22 12:06:43 +0100

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,34 @@
+<template>
+  <Layout>
+
+    <section>
+      <v-container>
+        <v-row>
+          <v-col sm="6">
+            <g-image alt="Profile image" src="~/profile.jpg" width="400" />
+          </v-col>
+          <v-col sm="6">
+            <h1>Hi, I'm Alexander</h1>
+            <p>I'm a web developer...</p>
+          </v-col>
+        </v-row>
+      </v-container>
+      
+    </section>
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Hello, world!'
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
